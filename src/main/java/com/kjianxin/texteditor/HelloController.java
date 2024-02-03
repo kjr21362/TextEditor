a87bb92c9c301bd916d976cf51633057dbe24957
kjr21362 <kjr21362@gmail.com>
2024-02-02 18:04:11 -0800

    [UI] Add textarea and listview

--- src/main/java/com/kjianxin/texteditor/HelloController.java
+++ src/main/java/com/kjianxin/texteditor/HelloController.java
@@ -1,17 +1,8 @@
 package com.kjianxin.texteditor;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 /**
  * HelloController. To add more details.
  */
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
